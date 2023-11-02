73b9e1e9f0a9e1c54395984372230c4cbe750e67
adriano.souza <adriano.souza@zpesystems.com>
2023-11-02 11:32:52 -0300

    feat: Refactor

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,18 +1,20 @@
 import './index.css'
 
 const Footer = ({ items }) => {
-  const qtditems = items.length
+  console.log('items: ', items)
+  const qtdItems = items.length
   const qtdSaved = items.reduce((acc, item) => {
     if (item.saved) {
       acc++
     }
     return acc
   }, 0)
-  const qtdSavedPercentage = Math.floor((qtdSaved / qtditems) * 100)
+  const qtdSavedPercentage =
+    qtdSaved > 0 ? Math.floor((qtdSaved / qtdItems) * 100) : 0
   return (
     <footer>
       <p>
-        Você tem {qtditems} items na lista e já guardou {qtdSaved} (
+        Você tem {qtdItems} items na lista e já guardou {qtdSaved} (
         {qtdSavedPercentage}%)
       </p>
     </footer>
